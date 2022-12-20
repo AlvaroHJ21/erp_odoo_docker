b24eafe420484c4fbb9f86ec02ccc533ad8c69d1
AlvaroHJ <alvarohuaysara@gmail.com>
2022-12-20 09:29:56 -0500

    siuu school

--- addons/school/models/teacher.py
+++ addons/school/models/teacher.py
@@ -3,4 +3,6 @@ from odoo import models, fields
 class Teacher(models.Model):
     _name = 'teacher'
     name = fields.Char(string='Nombre', required=True)
+    email = fields.Char(string='Email', required=True)
+    phone = fields.Char(string='Telefono', required=True)
     # subject_ids = fields.Many2many('school.subject', string='Materias')
