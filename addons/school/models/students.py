ee243bdeed65c6263ae1c906b36d1f62c7b4b9d4
leav1998 <luis.avila8@unmsm.edu.pe>
2022-12-19 21:03:44 -0500

    pipipi

--- addons/school/models/students.py
+++ addons/school/models/students.py
@@ -1,7 +1,31 @@
 from odoo import models,fields
 
+class GeneroFormat:
+
+    MASCULINO = "Masculino"
+    FEMENINO = "Femenino"
+    OTRO = "Otro"
+
+    OPTIONS = [
+        MASCULINO,
+        FEMENINO,
+        OTRO,
+
+    ]
+    SELECTION = [
+        ("Masculino", MASCULINO),
+        ("Femenino", FEMENINO),
+        ("Otro", OTRO),
+    ]
+
 class Student(models.Model):
     _name  = 'student.add'
 
     name= fields.Char(string="student Name")
     age = fields.Integer()
+
+    genero = fields.Selection(
+        selection=GeneroFormat.SELECTION, 
+        default=GeneroFormat.MASCULINO,
+        string="Formato"
+    )
